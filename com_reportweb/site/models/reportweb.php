600b234030678755173c9c3970c06af5f4f78630
kankan513 <kankan513@hotmail.com>
2015-02-19 07:43:13 +0700

    update2

---
+++ com_reportweb/site/models/reportweb.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * HelloWorld Model
+ *
+ * @since  0.0.1
+ */
+class ReportWebModelReport extends JModelItem
+{
+	/**
+	 * @var string message
+	 */
+	protected $message;
+ 
+	/**
+	 * Get the message
+	 * @return  string  The message to be displayed to the user
+	 */
+	public function getMsg()
+	{
+		if (!isset($this->message))
+		{
+			$this->message = 'Hello World!';
+		}
+		return $this->message;
+	}
+}
