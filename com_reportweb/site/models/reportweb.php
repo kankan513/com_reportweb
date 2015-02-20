f223f46f064345ca4c9dcf288f5a258edc897887
kankan513 <kankan513@hotmail.com>
2015-02-21 01:36:16 +0700

    update3

--- com_reportweb/site/models/reportweb.php
+++ com_reportweb/site/models/reportweb.php
@@ -15,23 +15,59 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class ReportWebModelReport extends JModelItem
+class ReportWebModelReportWeb extends JModelItem
 {
 	/**
-	 * @var string message
+	 * @var array messages
 	 */
-	protected $message;
+	protected $messages;
+ 
+	/**
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $type    The table name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  JTable  A JTable object
+	 *
+	 * @since   1.6
+	 */
+	public function getTable($type = 'ReportWeb', $prefix = 'ReportWebTable', $config = array())
+	{
+		return JTable::getInstance($type, $prefix, $config);
+	}
  
 	/**
 	 * Get the message
-	 * @return  string  The message to be displayed to the user
+	 *
+	 * @param   integer  $id  Greeting Id
+	 *
+	 * @return  string        Fetched String from Table for relevant Id
 	 */
-	public function getMsg()
+	public function getMsg($id = 1)
 	{
-		if (!isset($this->message))
+		if (!is_array($this->messages))
 		{
-			$this->message = 'Hello World!';
+			$this->messages = array();
 		}
-		return $this->message;
+ 
+		if (!isset($this->messages[$id]))
+		{
+			// Request the selected id
+			$jinput = JFactory::getApplication()->input;
+			$id     = $jinput->get('id', 1, 'INT');
+ 
+			// Get a TableHelloWorld instance
+			$table = $this->getTable();
+ 
+			// Load the message
+			$table->load($id);
+ 
+			// Assign the message
+			$this->messages[$id] = $table->greeting;
+		}
+ 
+		return $this->messages[$id];
 	}
 }
